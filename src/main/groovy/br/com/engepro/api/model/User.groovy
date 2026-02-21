759f120772cdac31ba5c93016b5dc627271977c9
João Manolo <joao.manolo@gmail.com>
2026-02-21 14:45:29 -0300

    Restringe funis por usuário não administrativo

--- src/main/groovy/br/com/engepro/api/model/User.groovy
+++ src/main/groovy/br/com/engepro/api/model/User.groovy
@@ -1,6 +1,10 @@
 package br.com.engepro.api.model
 
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.JoinTable
+import jakarta.persistence.ManyToMany
 import jakarta.persistence.Transient
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
@@ -21,6 +25,12 @@ class User extends BaseEntity implements UserDetails {
     Boolean hasLevelConfig = false
 
     LocalDateTime lastLogin
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_funnel_access",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "funnel_id"))
+    Set<Funnel> allowedFunnels = [] as Set
 
 
     @Override
