46b076b5a6d8982ba4a24bb3cf9f6e69522d77cb
João Manolo <manolo@blanq.com.br>
2026-02-22 20:37:02 -0300

    migrações

--- src/main/resources/db/migration/V2026_02_21_12_00__user_funnel_access.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user_funnel_access
-(
-    user_id   BIGINT(20) NOT NULL,
-    funnel_id BIGINT(20) NOT NULL,
-    PRIMARY KEY (user_id, funnel_id),
-    CONSTRAINT fk_user_funnel_access_user FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE NO ACTION,
-    CONSTRAINT fk_user_funnel_access_funnel FOREIGN KEY (funnel_id) REFERENCES funnel (id) ON DELETE CASCADE ON UPDATE NO ACTION
-);
